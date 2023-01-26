9c78d8b54ae505526ecc70caa6c6a1059ebc18d0
Kladbm <hollykiller123@gmail.com>
2023-01-27 00:07:17 +0200

    Initial commit

--- src/main/java/org/example/dao/EmployeeDAOImpl.java
+++ src/main/java/org/example/dao/EmployeeDAOImpl.java
@@ -3,7 +3,6 @@ package org.example.dao;
 import org.example.entity.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
